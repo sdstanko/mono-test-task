b748e773bd572b9bae15b3d741412031e12a5c5f
Daniel Stanko <stankodanijel@gmail.com>
2023-11-16 12:42:27 +0100

    made and styled List and ListItem

--- client/src/components/ListItem/index.jsx
+++ client/src/components/ListItem/index.jsx
@@ -1,8 +1,26 @@
-import React from "react";
-import styles from './ListItem.module.css'
+import React from 'react';
+import Button from '../UI/Button';
+import styles from './ListItem.module.css';
 
 const ListItem = () => {
-  return <div>ListItem</div>;
+    return (
+        <div className={styles.item}>
+            <div className={styles.photo}>
+                <img
+                    className={styles.picture}
+                    src="https://www.motortrend.com/uploads/2023/05/2024-bmw-5-series-17.jpeg?fit=around%7C875:492"
+                    alt=""
+                />
+            </div>
+            <div className={styles.item__body}>
+                <div className={styles.item__text}>
+                    <span className={styles.title}>BMW</span>
+                    <span className={styles.subtitle}>5 series</span>
+                </div>
+                <Button>Edit</Button>
+            </div>
+        </div>
+    );
 };
 
 export default ListItem;
--- client/src/pages/Models/index.jsx
+++ client/src/pages/Models/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import List from '../../components/List';
+import List from '../../components/Grid';
 import ListItem from '../../components/ListItem';
 import styles from './Models.module.css'
 
@@ -7,6 +7,14 @@ const Models = () => {
     return (
         <div className={styles.models}>
             <List>
+              <ListItem/>
+              <ListItem/>
+              <ListItem/>
+              <ListItem/>
+              <ListItem/>
+              <ListItem/>
+              <ListItem/>
+              <ListItem/>
               <ListItem/>
             </List>
         </div>
